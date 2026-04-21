235f61034bb2b549f30a9e245f95491dc6480f41
AllisonMRT <allisongorao2@gmail.com>
2026-04-20 22:45:53 -0300

    feat: exercicio 01

---
+++ pratica_02/exercicio01.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um número inteiro: "))
+if numero > 0:
+    print("o número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else :
+    print("O número é zero.")
